a9ca5b321162d5cd347413e55017b070e1ebabd8
nicolas.mormiche <nicolas160796@gmail.com>
2021-09-27 09:21:09 +0200

    Ajout de Book et Author

---
+++ src/Entity/Book.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\BookRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=BookRepository::class)
+ */
+class Book
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $author;
+
+    /**
+     * @ORM\Column(type="datetime_immutable")
+     */
+    private $releaseAt;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getAuthor(): ?string
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(string $author): self
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    public function getReleaseAt(): ?\DateTimeImmutable
+    {
+        return $this->releaseAt;
+    }
+
+    public function setReleaseAt(\DateTimeImmutable $releaseAt): self
+    {
+        $this->releaseAt = $releaseAt;
+
+        return $this;
+    }
+}
